893317d66c752a33357e86ab3f34177a808d16b6
Hugusse <huguesbroudin@gmail.com>
2024-12-02 18:31:49 +0100

    Nouveaux defs class player

--- entities.py
+++ entities.py
@@ -2,16 +2,23 @@ import pygame
 pygame.init()
 
 class Player:
-    def __init__(self, image, speed):
+    def __init__(self,spawn, image, speed):
         self.speed = speed
         self.image = image
-        self.pos = self.image.get_rect()
+        self.pos = self.image.get_rect().move(spawn)
+        self.state = 'still'
+
+    def reinit(self,spawn):
+        self.pos = self.image.get_rect().move(spawn)
+        self.state = 'still'
+
     def move(self,x,y):
         self.pos = self.pos.move(self.speed*x,self.speed*y)
+        self.state = 'moving'
 
 
 class Rat:
-    speed = 10
+    speed = 1
     def __init__(self, length, height,image):
         self.image = image
         self.pos = self.image.get_rect().move(length, height)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys, pygame,os
+from pygame.locals import *
 print("Répertoire courant :", os.getcwd())
 pygame.init()
 
@@ -8,11 +9,12 @@ import entities
 screen = pygame.display.set_mode((640,480))
 player = pygame.image.load( os.path.join("Ressources", "Joueur.png")).convert()
 background = pygame.image.load( os.path.join("Ressources", "BG.png")).convert()
+eimage = pygame.image.load(os.path.join("Ressources", "Rat.png")).convert()
 screen.blit(background,(0,0))
 
 aliveenemies = []
 
-eimage = pygame.image.load(os.path.join("Ressources", "Rat.png")).convert()
+p = entities.Player((320,240),player,2)
 
 for i in range(10) : #Initialise 10 rats
     e = entities.Rat(i,20*i, eimage)
@@ -23,11 +25,16 @@ while 1:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == KEYDOWN:
+            if event.key == K_UP:
+                print("key up")
+
     for e in aliveenemies:
         screen.blit(background,(0,0))
     for e in aliveenemies:
         e.move()
         screen.blit(e.image, e.pos)
+    screen.blit(p.image,p.pos)
     pygame.display.update()
     pygame.time.delay(50)
     screen.fill((0,0,0))
